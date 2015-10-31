9ced1ab91bba149632c4831f059402f9498b0585
wherby <187225577@qq.com>
2015-10-31 20:43:51 +0800

    ActiveRecord-1.finished

--- ActiveRecord/todolists/assignment/assignment.rb
+++ ActiveRecord/todolists/assignment/assignment.rb
@@ -10,12 +10,14 @@ class Assignment
       # accept a hash of user properties (`:username` and `:password_digest`) as an input parameter. Note these are 100% same as model class.
       # use the User Model class to create a new user in the DB
       # return an instance of the class with primary key (`id`), and dates (`created_at` and `updated_at`) assigned
+      User.create(params)
   end
 
   def create_todolist(params)
       # accept a hash of todolist properties (`:name` and `:due_date`) as an input parameter. Note these are not 100% the same as Model class.
       # use the TodoList Model class to create a new user in the DB
       # return an instance of the class with primary key (`id`), and dates (`created_at` and `updated_at`) assigned
+      TodoList.create(list_name:params[:name],list_due_date:params[:due_date])
   end
 
   #
@@ -25,12 +27,14 @@ class Assignment
       # accept offset and input input parameters
       # use the User Model class to find all Users, ordered by `updated_at` ascending, with specified row offset and row limit
       # return a collection of User instances that represent the specified page
+      User.order(:updated_at).offset(offset).limit(limit)
   end
 
   def find_alllists(offset, limit)
       # accept offset and limit input parameters
       # use the TodoList Model class to find all TodoLists, ordered by `list_due_date` descending, with specified row offset and row limit
       # return a collection of TodoList instances that represent the specified page
+      TodoList.order(list_due_date: :desc).offset(offset).limit(limit)
   end
 
   #
@@ -40,12 +44,14 @@ class Assignment
       # accept a username input parameter
       # use the User Model class to find all Users with the supplied username. Note that we have not yet constrained the username to be unique.
       # return a collection of User instances that match the provided username
+      User.where(username:  username)
   end 
 
   def find_todolist_byname(name)
       # accept a name input parameter
       # use the TodoList Model class to find all TodoLists with the supplied list_name. Note that list_name is not required to be unique.
       # return a collection of TodoList instances that match the provided name
+      TodoList.where(list_name: name)
   end     
 
   #
@@ -55,12 +61,14 @@ class Assignment
       # accept an id input parameter
       # use the User Model class to get the User associated with the `id` primary key
       # return the User instance that matches the provided id
+      User.find(id)
   end
 
   def get_todolist_byid(id)
       # accept an id input parameter
       # use the TodoList Model class to get the TodoList associated with the `id` primary key
       # return the TodoList instance that matches the provided id
+      TodoList.find(id)
   end
 
   #
@@ -70,12 +78,14 @@ class Assignment
       # accept an id and password_digest input parameters
       # use the User Model class to update the `password_digest` for the User associated with the id primary key
       # (no return is required)
+      User.find(id).update(password_digest: password_digest) 
   end
 
   def update_listname(id, name)
       # accept an id and name input parameters
       # use the TodoList Model class to update the `list_name` for the TodoList associated with id primary key 
       # (no return is required)
+      TodoList.find(id).update(list_name: name)
   end 
 
   #
@@ -85,11 +95,13 @@ class Assignment
       # accept an id input parameter
       # use the User Model class to remove the User associated with the `id` primary key from the database
       # (no return is required)
+      User.delete(id)
   end 
 
   def delete_todolist(id)
       # accept an id input parameter
       # use the TodoList Model class to get the TodoList associated with the `id` primary key.
       # (no return is required)
+      TodoList.delete(id)
   end
 end
