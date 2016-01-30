64d763ad8a77f9611bc6b894bd76751bdd8f9857
wherby <187225577@qq.com>
2016-01-30 15:41:41 +0800

    add all

--- course03/module01/raceday/app/models/Racer.rb
+++ course03/module01/raceday/app/models/Racer.rb
@@ -6,4 +6,20 @@ class Racer
 	def self.collection
 		self.mongo_client['racers']
 	end
+	
+	def self.all(prototype={}, sort={:number => 1},skip=0,limit=nil)
+		tmp = {}
+		sort.each {|k,v| 
+      		k = k.to_sym==:population ? :pop : k.to_sym
+      		tmp[k] = v  if [:number, :first_name, :last_name, :gender, :group,:secs].include?(k)
+    	}
+		sort=tmp
+		prototype=prototype.symbolize_keys.slice(:number, :first_name, :last_name, :gender, :group,:secs) if !prototype.nil?
+		result=collection.find(prototype)
+          .projection({number:true, first_name:true, last_name:true, gender:true,group:true,secs:true})
+          .sort(sort)
+          .skip(skip)	
+		result=result.limit(limit) if !limit.nil?
+		return result
+	end
 end	
